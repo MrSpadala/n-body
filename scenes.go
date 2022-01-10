74d7d46b21b9098f01416c9f9386e1e21c7d3305
Pietro Spadaccino <pietro.spa@gmail.com>
2022-01-10 16:12:31 +0100

    rotating disc scene

---
+++ scenes.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func RotatingDisc(radius float64, cx float64, cy float64, w float64, n_particles uint64) []body {
+	// w angular velocity, in rad/s
+	fmt.Println("Generating rotating disc scene...")
+	res := make([]body, n_particles)
+	rand := rand.New(rand.NewSource(42))
+	for i := uint64(0); i < n_particles; i++ {
+		r := rand.Float64() * radius
+		theta := rand.Float64() * 2 * math.Pi
+		v := w * r
+
+		x := cx + r*math.Cos(theta)
+		y := cy + r*math.Sin(theta)
+		vx := - v * math.Sin(theta)
+		vy := v * math.Cos(theta)
+
+		res[i] = body{
+			x:    x,
+			y:    y,
+			vx:   vx,
+			vy:   vy,
+			mass: 1}
+	}
+	return res
+}
