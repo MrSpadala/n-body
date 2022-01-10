09db8e32a6f416641a01576a84fc79f25efab683
Pietro Spadaccino <pietro.spa@gmail.com>
2022-01-10 16:12:14 +0100

    fix cloud colors

--- draw.go
+++ draw.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	body_size = 1
+	body_size = 0
 )
 
 func drawAll(x_start float64, x_end float64, y_start float64, y_end float64) {
@@ -57,7 +57,7 @@ func drawStep(i_step uint64, worker_c chan bool, x_start float64, x_end float64,
 	json.Unmarshal(data, &bodies_json)
 
 	// Mantain a separate matrix for pixels, since image has no Get method
-	pixels := [(h+1) * (w+1)]color.RGBA{}
+	pixels := [(h + 1) * (w + 1)]color.RGBA{}
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{w, h}
@@ -83,7 +83,7 @@ func drawStep(i_step uint64, worker_c chan bool, x_start float64, x_end float64,
 		const c_step = uint8(250)
 		for dx := -body_size; dx <= body_size; dx++ {
 			for dy := -body_size; dy <= body_size; dy++ {
-				i_pix := (x+dx)*h+y+dy
+				i_pix := (x+dx)*h + y + dy
 				if i_pix < 0 || i_pix >= (h+1)*(w+1) {
 					continue
 				}
@@ -93,14 +93,14 @@ func drawStep(i_step uint64, worker_c chan bool, x_start float64, x_end float64,
 						if pix.R == 0xff {
 							// do nothing
 						} else {
-							if pix.R+c_step > 0xff {
+							if pix.R+c_step < pix.R {
 								pix.R = 0xff
 							} else {
 								pix.R += c_step
 							}
 						}
 					} else {
-						if pix.G+c_step > 0xff {
+						if pix.G+c_step < pix.G {
 							pix.R = pix.G + c_step - 0xff
 							pix.G = 0xff
 						} else {
@@ -108,7 +108,7 @@ func drawStep(i_step uint64, worker_c chan bool, x_start float64, x_end float64,
 						}
 					}
 				} else {
-					if pix.B+c_step > 0xff {
+					if pix.B+c_step < pix.B {
 						pix.G = pix.B + c_step - 0xff
 						pix.B = 0xff
 					} else {
